d381632ab61562eae33f05ca92bd43db53e4c87e
chanakaHetti <cdileep40@gmail.com>
2021-12-03 01:39:43 +0000

    Development

--- djcrm/asgi.py
+++ djcrm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_first project.
+ASGI config for djcrm project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_first.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djcrm.settings')
 
 application = get_asgi_application()
