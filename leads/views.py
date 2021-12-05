c1d378ce7819d794f5bfdb4fe28fa2e5e59321da
chanakaHetti <cdileep40@gmail.com>
2021-12-05 00:26:12 +0000

    Lead app development

---
+++ leads/views.py
@@ -0,0 +1,22 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
+
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+
+    # return HttpResponse("Hello world")
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_details.html", context)
